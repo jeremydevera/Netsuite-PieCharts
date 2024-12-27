40235674ff6c4eec7d037b02ce16eb6dd99c9dad
Jeremy Adrian <38548937+jeremydevera@users.noreply.github.com>
2024-12-27 18:04:37 -0500

    test
    test

---
+++ amcharts_sl_test.js
@@ -0,0 +1,23 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Suitelet
+ */
+define(['N/file'], function (file) {
+    function onRequest(context) {
+        if (context.request.method === 'GET') {
+            // Load the HTML file from the File Cabinet
+            var htmlFile = file.load({
+                id: 'SuiteScripts/amcharts_piechart.html' // Adjust path as needed
+            });
+
+            // Send the content of the HTML file as the response
+            context.response.write({
+                output: htmlFile.getContents()
+            });
+        }
+    }
+
+    return {
+        onRequest: onRequest
+    };
+});
